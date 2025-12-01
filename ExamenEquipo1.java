863b8be31a7c4a6868921126e1c259c752763917
ManuelDLRS <up240399@alumnos.upa.edu.mx>
2025-12-01 09:36:56 -0600

    Correccion checkstyle

--- ExamenEquipo1.java
+++ ExamenEquipo1.java
@@ -1,6 +1,10 @@
+
+/**
+ * Clase main para el examen equipo 1.
+ */
 public class ExamenEquipo1 {
 
-    public static void main(String[] args) {
-        System.out.println("Vamos a chambear mi gentee");
-    }
+  public static void main(String[] args) {
+    System.out.println("Vamos a chambear mi gentee");
   }
+}
